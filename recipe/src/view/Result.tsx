e4c743fa7fa579309fd4352917225319582e4ce1
Jun Koyama <a23z1jk@aiit.ac.jp>
2023-11-17 02:49:42 +0000

    fix: refactor.

--- recipe/src/view/Result.tsx
+++ recipe/src/view/Result.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
 import './Result.css';
 import { useLocation } from 'react-router-dom';
 
@@ -12,10 +12,10 @@ const SearchResultPage = () => {
     <div>
       <h2>検索結果</h2>
       <ul>
-        {recipes.map((recipe) => (
+        {recipes.map((recipe: { recipe: { uri: Key | null | undefined; label: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; image: string | undefined; source: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; calories: number; }; }) => (
           <li key={recipe.recipe.uri}>
             <h3>{recipe.recipe.label}</h3>
-            <img src={recipe.recipe.image} alt={recipe.recipe.label} />
+            <img src={recipe.recipe.image} />
             <p>{recipe.recipe.source}</p>
             <p>カロリー: {recipe.recipe.calories.toFixed(2)} kcal</p>
           </li>
